ffc72134d5f7dd79c83aa93323f7f6539cd2de04
Ranjith <ranjith.19it@kct.ac.in>
2021-09-28 20:42:21 +0530

    changes 3

--- main.js
+++ main.js
@@ -11,6 +11,10 @@ $(document).ready(function(){
     $('.login-form form .fa-times').click(function(){
         $('.login-form').removeClass('popup');
     });
+    $('.login-form .sign-in-with-google p').click(function(event){
+        event.preventDefault();
+        signin() 
+    });
     $(window).on('load scroll',function(){
             $('#menu').removeClass('fa-times');
             $('.navbar').removeClass('nav-toggle');
@@ -28,3 +32,43 @@ $(document).ready(function(){
             });
     });
 });
+const firebaseConfig = {
+    apiKey: "AIzaSyCtp-MR-gJKz8Qfeyuv4mYLqyJ-D1GdxIw",
+    authDomain: "e-learning-platform-f5d7c.firebaseapp.com",
+    projectId: "e-learning-platform-f5d7c",
+    storageBucket: "e-learning-platform-f5d7c.appspot.com",
+    messagingSenderId: "553563438819",
+    appId: "1:553563438819:web:67f0f732a4c7240df28fd6",
+    measurementId: "G-9WJJ7KFJTR"
+  };
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+function signin(){
+    console.log("hello")
+    var provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider).then((result) => 
+    {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      var token = credential.accessToken;
+      // The signed-in user info.
+      var user = result.user;
+      console.log(user)
+      // ...
+    }).catch((error) => {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // The email of the user's account used.
+      var email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      var credential = error.credential;
+      // ...
+    });}
+    firebase.auth().onAuthStateChanged(async (user) => {
+        if (user) {
+          console.log(user);
+          }
+        }
+      );
+
+  
